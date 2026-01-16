15b6aab8b7dad8da2529019f7cc716f8b1e6cd76
jtompkins1 <jennifermtompkins@gmail.com>
2026-01-15 21:21:22 -0600

    complete

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const routes = require('express').Router();
 const temple = require('./temple');
+//const swagger = require('./swagger');
+
+routes.use('/', require('./swagger'));
 
 routes.use('/temples', temple);
 routes.use(
